a710b08a2fb9813471a8d8c610e4eda2ec5e9a56
rocky1907 <alemanrodrigo1907@gmail.com>
2021-09-24 16:39:47 -0600

    Create Branch

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// middlewares, hace referencia a algunas funciones que se ejecutan antes de que lleguen a las rutas
+app.use(express.json());
+app.use(express.urlencoded({extends:false}));
+app.use(cors());
+
+//definicion de las rutas
+app.use(require('./apiServices/users/Controllers/Routes/users'));
+
+
+app.listen(3000);
+console.log("Serve on port 3000");
