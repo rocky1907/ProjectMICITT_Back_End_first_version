01e522da85cdab223c2e842e93dc4dc0e9434d2d
BritanyRojas <britanyrojas98@gmail.com>
2021-10-28 00:43:17 -0600

    contra

--- src/apiServices/users/routes.js
+++ src/apiServices/users/routes.js
@@ -3,15 +3,12 @@ const express = require('@awaitjs/express');
 const controller = require('../users/Controller');
 const router = express.Router();
 
-//const {getUsers, createUser, getUserById, deleteUser, updateUser} = require('../controllers/user.controller');
-
-//router.get('/user',controller.getUsers);
 
 router.post('/user', controller.addUser);
 router.get('/user', controller.getUsers);
+router.put('/user/:user_name', controller.changePasswordName);
+router.get('/user/:user_name', controller.getUserByUserName);
 
-//router.get('/user/:id',controller.getUserById);
-//router.delete('/user/:id',controller.deleteUserById);
-//router.put('/user/:id',controller.updateUserById);
+
 
 module.exports = router;
