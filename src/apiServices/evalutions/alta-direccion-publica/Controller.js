d9b1d6c1a6ed26ac3aa297c818b7c7f15e71f879
natashaom <nattiortega12@gmail.com>
2022-05-04 21:50:32 -0600

    evaluacionesPendientes

--- src/apiServices/evalutions/alta-direccion-publica/Controller.js
+++ src/apiServices/evalutions/alta-direccion-publica/Controller.js
@@ -22,11 +22,35 @@ const getBossSkillActions = async (req,res)=>{
     res.status(200).json(response.rows);
 }
 
+const getPendingEv = async (req,res)=>{
+    const response = await pool.query('select * from public."Evaluation" where status =\'Pendiente\';');
+    res.status(200).json(response.rows);
+}
+
+// const getMyEval = async (req,res)=>{
+//     const id_fun = req.params.id_fun;
+//     const response = await pool.query('select * from public."Evaluation" where status =\'Pendiente\' and id_fun = $1',[id_fun]);
+//     res.status(200).json(response.rows);
+// }
+
+const getPendingEvs = async (req,res)=>{
+    const pk_id_num = req.params.pk_id_num;
+    var pkAux = parseInt(pk_id_num);
+    console.log(pkAux);
+    const response = await pool.query('select * from public.listarEvaluacionesPen($1);',[pkAux]);
+    res.status(200).json(response.rows);
+}
+
+
 
 module.exports = {
     getDescriptions,
     getProfessionalSkills,
     getRubricEvaluation,
     getBossSkill,
-    getBossSkillActions
+    getBossSkillActions,
+    getPendingEv,
+   // getMyEval,
+    getPendingEvs
+
 }
--- src/apiServices/evalutions/alta-direccion-publica/routes.js
+++ src/apiServices/evalutions/alta-direccion-publica/routes.js
@@ -7,5 +7,7 @@ router.get('/alta-direccion-publica/weighted',controller.getProfessionalSkills);
 router.get('/alta-direccion-publica/competencies/rubric',controller.getRubricEvaluation);
 router.get('/alta-direccion-publica/competencies/bosskill',controller.getBossSkill);
 router.get('/alta-direccion-publica/competencies/bosskill/actions',controller.getBossSkillActions);
+router.get('/evaluations',controller.getPendingEv);
+router.get('/evaluations/:pk_id_num',controller.getPendingEvs);
 //router.post('/periods',controller.addPeriod);
 module.exports = router;
