1cca13aafe1122f3f6cdf0c079203c2e4c9c1c18
ulloadl <danielleonardour@gmail.com>
2022-05-05 02:01:34 -0600

    Back listas evaluaciones gen y fun

--- src/apiServices/evalutions/alta-direccion-publica/Controller.js
+++ src/apiServices/evalutions/alta-direccion-publica/Controller.js
@@ -22,11 +22,27 @@ const getBossSkillActions = async (req,res)=>{
     res.status(200).json(response.rows);
 }
 
+const getEvaVal = async (req,res)=>{
+    const status = "Validado";
+    const response = await pool.query('	select * from public."Evaluation" where status = $1',[status]);
+    res.status(200).json(response.rows);
+}
+
+
+const getEvaValFun = async (req,res)=>{
+    const id = req.params.id;
+    const status = "Validado";
+    const response = await pool.query('	select * from public."Evaluation" where id_fun = $1 and status = $2',[id, status]);
+    res.status(200).json(response.rows);
+}
+
 
 module.exports = {
     getDescriptions,
     getProfessionalSkills,
     getRubricEvaluation,
     getBossSkill,
-    getBossSkillActions
+    getBossSkillActions,
+    getEvaVal,
+    getEvaValFun
 }
--- src/apiServices/evalutions/alta-direccion-publica/routes.js
+++ src/apiServices/evalutions/alta-direccion-publica/routes.js
@@ -7,5 +7,9 @@ router.get('/alta-direccion-publica/weighted',controller.getProfessionalSkills);
 router.get('/alta-direccion-publica/competencies/rubric',controller.getRubricEvaluation);
 router.get('/alta-direccion-publica/competencies/bosskill',controller.getBossSkill);
 router.get('/alta-direccion-publica/competencies/bosskill/actions',controller.getBossSkillActions);
+router.get('/evaluationsVali/',controller.getEvaVal)
+router.get('/evaluationsValiFun/:id',controller.getEvaValFun)
+
+
 //router.post('/periods',controller.addPeriod);
 module.exports = router;
